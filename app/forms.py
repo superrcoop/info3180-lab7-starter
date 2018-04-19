fdb906de7286feafa40a5072f9cbfcf131f11fcd
Jordan Cooper <jordan.cooper3000@gmail.com>
2018-04-18 23:52:42 -0500

    add flask wtf upload form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,10 @@
+from wtforms import StringField, PasswordField, HiddenField, BooleanField, IntegerField
+from wtforms.validators import Required, Length, Email, EqualTo, DataRequired
+from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
+ALLOWED_EXTENSIONS = set(['txt','pdf','png', 'jpg', 'jpeg', 'gif'])
+
+class upload_Form(FlaskForm):
+    description = StringField('Description', validators=[Required('Please provide a description')])
+    upload = FileField('files[]', validators=[FileRequired(),FileAllowed(ALLOWED_EXTENSIONS, 'File not allowed')])
